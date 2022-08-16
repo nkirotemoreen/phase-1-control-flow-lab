784d706fb35ae77aea9fd9f72846e2889c9b8d72
moreenisaac <nkirotemoreen70@gmail.com>
2022-08-16 07:05:41 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+
+  let charges
+   //freeride for feet less than 400
+  if (feet <= 400) {
+    charges = "This one is on me!"
+  }
+  //pay 30 dollars for more than 2000 feet
+    else if (feet > 2000 && feet < 2500) {
+     charges = "I will gladly take your thirty bucks."
+}
+//does not allow more than 2500 feet
+    else {
+  charges = "No can do."
+}
+return charges;
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  return city === "NYC" ? 'Ok, sounds good.' : "No go."
+
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+  }
 }
